0fdf7408899ca2c789611db53a468ba2052b5962
Alexis <sixela295@gmail.com>
2021-12-15 17:52:54 -0500

    integrated non-.zip file upload rejection to drag file and pre-server merge code

--- src/components/SideBar/DragFile.jsx
+++ src/components/SideBar/DragFile.jsx
@@ -6,9 +6,20 @@ document.addEventListener('drop', (e) => {
   e.stopPropagation();
 
   for (const f of e.dataTransfer.files) {
-    console.log('File(s) you dragged here: ', f.path);
-    // post f.path to server, append file sync to models for connection pool start
-    alert('Success!');
+    if (f.path.includes('.zip')) {
+    //if path.extname = zip, 
+        console.log('File(s) you dragged here: ', f.path);
+        // post f.path to server, append file sync to models for connection pool start
+        /*fetch('/api/fileUpload', {
+            method: "POST",
+            headers: { 'Content-Type': 'application-json' },
+            body: JSON.stringify({ 'filePath': f.path }),
+        })
+            .then(res =>*/ alert('Upload success!')/*)
+            .catch(error => console.log(error))*/;
+    }
+    else {throw new Error('Upload must be a zip bundle file. Please check documentation or demo and try again.')}
+    //else return error -- must upload zip bundle file. Please check demo for more.
   }
 });
 document.addEventListener('dragover', (e) => {
@@ -16,38 +27,6 @@ document.addEventListener('dragover', (e) => {
   e.stopPropagation();
 });
     return(
-        <div id='holder'>Drag your file here</div>
+        <div id='holder'>Drag your bundle here</div>
     )
 }
-/*    <div id="holder">
-        Drag your Astra Secure Connection bundle here
-      </div>
-      
-      <script>
-        document.addEventListener('drop', (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-      
-          for (const f of e.dataTransfer.files) {
-            if (f.path.includes('.zip')) {
-            //if path.extname = zip, 
-                console.log('File(s) you dragged here: ', f.path);
-                // post f.path to server, append file sync to models for connection pool start
-                fetch('/api/fileUpload', {
-                    method: "POST",
-                    headers: { 'Content-Type': 'application-json' },
-                    body: JSON.stringify({ 'filePath': f.path }),
-                })
-                    .then(res => alert('Upload success!'))
-                    .catch(error => console.log(error));
-            }
-            else {throw new Error('Upload must be a zip bundle file. Please check documentation or demo and try again.')}
-            //else return error -- must upload zip bundle file. Please check demo for more.
-          }
-        });
-        document.addEventListener('dragover', (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        });
-      </script></div>
-      */
