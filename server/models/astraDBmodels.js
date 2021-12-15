03adf8bb74a368b5c2a8188447180688462018d6
Alexis <sixela295@gmail.com>
2021-12-15 10:22:03 -0500

    commiting before new dev merge

--- server/models/astraDBmodels.js
+++ server/models/astraDBmodels.js
@@ -1,27 +1,34 @@
+const cassandra = require('cassandra-driver');
+const distance = cassandra.types.distance;
+const path = require('path');
+const fs = require('fs');
 
-const { createClient } = require("@astrajs/collections");
+// const options = {
+//   contactPoints,
+//   localDataCenter,
+//   pooling: {
+//     coreConnectionsPerHost: {
+//       [distance.local]: 2,
+//       [distance.remote]: 1
+//     } 
+//   }
+// };
+// use path. resolve to search for a file with secure-connect in its name OR take state of file path from upload?
+// --> * zip to get wildcard and search for any zip file.
+const pathRel = path.resolve(__dirname, '*.zip');
 
-let astraClient = null;
+const client = new cassandra.Client( 
+  {
+  cloud: { secureConnectBundle: pathRel },
+  credentials: { username: 'rKhcZkZjAGkSbWZTxorLYUcN', password: 'yIESLvsNB2qke-IM.BZ2ft.Qvs40KO-,Xsz6.q_rxIyhHvy5iQNXlDH,Qqc4ywExP_ZYAMjaduYIMw4,Hnu7Wk-T7vWFhPZ.,vgRz+gdBjfs3D0ZUo,DeC-gKYQ3hv,l' }
+});
 
-const getAstraClient = async () => {
-  if (astraClient === null) {
-    astraClient = await createClient(
-      {
-        astraDatabaseId: process.env.ASTRA_DB_ID,
-        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
-      },
-      30000
-    );
-  }
-  return astraClient;
-};
+/*
+Even though calling connect() is not required (the execute() method internally calls to connect), 
+it is recommended you call to #connect() on application startup, this way you can ensure that you start your app once 
+your are connected to your cluster.
+*/
 
-const getCollection = async () => {
-  const documentClient = await getAstraClient();
-  return documentClient
-    .namespace(process.env.ASTRA_DB_KEYSPACE)
-    // .collection("sampleDB");
-};
+client.connect();
 
-module.exports = { getAstraClient, getCollection };
+module.exports = client;
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const db = require('client');
+const cassandra = require('cassandra-driver');
+const path = require('path');
+const fs = require('fs');
+
+const astraCreate = require('astraCreateController.js');
+const astraSync = require('astraSyncController.js');
 
 const router = express.router();
 
