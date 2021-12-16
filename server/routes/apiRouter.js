0b246a0f17fb3cc61751ecb60274c4b7f15b4f19
Alexis <sixela295@gmail.com>
2021-12-16 12:42:00 -0500

    added server files from closed branch, added fetch. currently receiving 504 error on proxy

---
+++ server/routes/apiRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('client');
+const cassandra = require('cassandra-driver');
+const path = require('path');
+const fs = require('fs');
+
+const astraCreate = require('astraCreateController.js');
+const astraSync = require('astraSyncController.js');
+const fileUpload = require('fileUploadController.js');
+
+const router = express.router();
+
+router.post('/fileUpload', fileUpload.upload, (req, res) => {
+  res.response(200);
+})
+
+module.exports = router;
