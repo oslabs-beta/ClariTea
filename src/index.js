aa1d301fe9dc47c3a6fcffa27d8cdbe33ec8addb
peyton333 <pdpedersen333@gmail.com>
2021-12-30 16:53:51 -0600

    Add changes to mock data in tree component, added replication factor tree component with mock data

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.scss';
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Tree from './components/views/Tree.jsx'
 import MainContainer from './components/Container/MainContainer.jsx'
-
+import RepTree from './components/views/ReplicationFactor.jsx'
 
 //added an index.html instead of creating a div here and calling it root..added path to htmlplugin...
     //logic: i think we'll need script tags for d3?
@@ -16,6 +16,7 @@ render(
 <Routes>
     <Route path="/" element={<MainContainer/>}></Route>
     <Route exact path = "/tree" element={<Tree/>}></Route>
+    <Route exact path = "/repTree" element={<RepTree/>}></Route>
 </Routes>
 </HashRouter>
 , document.getElementById('root'))
