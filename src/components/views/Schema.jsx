0c7e215e1dea4eb3faf967cc9a2691fae508b61f
Kirsten Blair <kirstenblair@kirstens-mbp.lan>
2022-01-03 13:33:43 -0500

    schema snapshot charting completed, unstyled table

--- src/components/views/Schema.jsx
+++ src/components/views/Schema.jsx
@@ -7,22 +7,115 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-/****CQL DATA TYPES TABLE****/
-function createDataTypes(name, genre, title, releasedate, avgrating) {
+// /****CQL DATA TYPES TABLE****/
+// function createDataTypes(name, genre, title, releasedate, avgrating) {
+//   return { name, genre, title, releasedate, avgrating };
+// }
+
+// const rows = [
+//   createDataTypes('Data Type', 'text', 'text', 'date', 'blob'),
+// ];
+
+// export default function DataTypeTable() {
+//   return (
+//     <TableContainer component={Paper}>
+//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//         <TableHead>
+//           <TableRow>
+//             <TableCell><h1>CQL DATA TYPES</h1></TableCell>
+//             <TableCell align="center">Movie Genre</TableCell>
+//             <TableCell align="center">Movie Title</TableCell>
+//             <TableCell align="center">Release Date</TableCell>
+//             <TableCell align="center">Average Rating</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rows.map((row) => (
+//             <TableRow
+//               key={row.name}
+//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//             >
+//               <TableCell component="th" scope="row">
+//                 {row.name}
+//               </TableCell>
+//               <TableCell align="center">{row.genre}</TableCell>
+//               <TableCell align="center">{row.title}</TableCell>
+//               <TableCell align="center">{row.releasedate}</TableCell>
+//               <TableCell align="center">{row.avgrating}</TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
+
+// /****CQL DATA MODELS TABLE****/
+// function createDataModels(name, genre, title, releasedate, avgrating) {
+//   return { name, genre, title, releasedate, avgrating };
+// }
+
+// const rowsType = [
+//   createDataModels('Data Models', 'partition key', 'clustering key', 'N/A', 'N/A'),
+// ];
+
+// export default function DataModelTable() {
+//   return (
+//     <TableContainer component={Paper}>
+//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//         <TableHead>
+//           <TableRow>
+//             <TableCell><h1>CQL DATA MODELS</h1></TableCell>
+//             <TableCell align="center">Movie Genre</TableCell>
+//             <TableCell align="center">Movie Title</TableCell>
+//             <TableCell align="center">Release Date</TableCell>
+//             <TableCell align="center">Average Rating</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rowsType.map((row) => (
+//             <TableRow
+//               key={row.name}
+//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//             >
+//               <TableCell component="th" scope="row">
+//                 {row.name}
+//               </TableCell>
+//               <TableCell align="center">{row.genre}</TableCell>
+//               <TableCell align="center">{row.title}</TableCell>
+//               <TableCell align="center">{row.releasedate}</TableCell>
+//               <TableCell align="center">{row.avgrating}</TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
+
+/****CQL SCHEMA Snapshot****/
+function createDataSnapshot(name, genre, title, releasedate, avgrating) {
   return { name, genre, title, releasedate, avgrating };
 }
 
 const rows = [
-  createDataTypes('Data Type', 'text', 'text', 'date', 'blob'),
+  createDataSnapshot('Data Types', 'text', 'text', 'date', 'blob'),
+  createDataSnapshot('Data Models', 'partition key', 'clustering key', 'N/A', 'N/A'),
+  createDataSnapshot('Node1', 'Action', 'No Time To Die', '10-08-2021', '87%'),
+  createDataSnapshot('Node2', 'Animation', 'Your Name', '04-07-2017', '96%'),
+  createDataSnapshot('Node3', 'Comedy', 'Grown Ups', '06-25-2010', '90%'),
+  createDataSnapshot('Node4', 'Drama', 'Little Women', '12-25-2019', '90%'),
+  createDataSnapshot('Node5', 'Sci-Fi', 'The Matrix: Resurrections', '12-22-2021', '63%'),
+  createDataSnapshot('Node6', 'Thriller', 'I Care A Lot', '02-19-2021', '58%'),
 ];
 
-export default function DataTypeTable() {
+export default function DataSnapshotTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell><h1>CQL DATA TYPES</h1></TableCell>
+            <TableCell><h1>CQL SCHEMA MODELS</h1></TableCell>
             <TableCell align="center">Movie Genre</TableCell>
             <TableCell align="center">Movie Title</TableCell>
             <TableCell align="center">Release Date</TableCell>
