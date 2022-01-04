5be60584fc0e2d5c7c43f59ac56668cadffd12f8
Alexis <sixela295@gmail.com>
2022-01-04 17:15:47 -0500

    created enzyme test file with adapter alternative

---
+++ __tests__/enzyme.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import toJson from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
