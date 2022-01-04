4ed94fa41f6c69a01dbd5a6ad7ef9b3df1b28d56
britlim <britsta92@gmail.com>
2022-01-04 11:18:11 -0800

    added logo to header, added assets folder and logo file

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,22 +4,25 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Image } from '@mui/icons-material';
+import logoblack from '../assets/logoblack.png'
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            ClariTea
-          </Typography>
+      <AppBar position="static" style={{paddingLeft: "0px"}}> 
+        <Toolbar className="toolbar-header" style={{paddingLeft: "0px"}}>
+        <div>
+        <img id="logoHeader" src={logoblack} />
+        </div>
+        <div>
           <Button color="inherit" style={{textDecoration: 'none', color: 'white'}}>
           <a style={{ display: 'flex', textDecoration: 'none', alignItems: 'center', color: 'white'}} color="inherit" href="https://github.com/oslabs-beta/cassandra-visualizer" target="_blank">
               <i className="fab fa-github fa-3x" style={{ fontSize: "30px"}}></i>
               <p>Github</p>
             </a>
           </Button>
-          <Button href="" color="inherit">Tutorial</Button>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
